18f31e48c11370d80266201dc3af7a8a8961aa59
Venkatesh Muniyandi <venkatesh_muniyandi@dialog.com.au>
2017-08-11 07:43:05 +1000

    Commiting draft version

---
+++ LiquibaseConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using cw = System.Console;
+
+namespace LiquibaseConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            cw.WriteLine("_______________________________________________________________");
+            cw.WriteLine("                       LIQUIBASE UPGRADE                       ");
+            cw.WriteLine("_______________________________________________________________");
+            cw.WriteLine("Environments:");
+            cw.WriteLine("\t1-DEV");
+            cw.WriteLine("\tL1: LX1");
+            cw.WriteLine("\tL2: LX2");
+            cw.WriteLine("\tL3: LX3");
+            cw.WriteLine("\tL4: LX4");
+            cw.WriteLine("\tL9: LX9");
+            cw.WriteLine("\tL8: LX8");
+            cw.WriteLine("Select your Environment: ");
+            string selectedEnvironment  = cw.ReadLine();
+            cw.WriteLine($"Thanks for selecting Environment :{selectedEnvironment}");
+            cw.WriteLine($"Updating Database .....");
+            for (int i = 0; i <= 10; i++) { 
+                Thread.Sleep(1000);
+                Console.Write(".");
+            }
+            cw.WriteLine($"Liquibase Successfully Updated....");
+            cw.ReadKey();
+        }
+    }
+}
---
+++ ProcessExecuteWithInput/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessExecuteWithInput
+{
+
+    public class ExecuteLiquibase
+    {
+        ProcessStartInfo processInfo = new System.Diagnostics.ProcessStartInfo(@"C:\Users\venkat\Documents\Visual Studio 2017\Projects\LiquibaseConsoleApp\LiquibaseConsoleApp\bin\Release\LiquibaseConsoleApp.exe");
+        Process p = new Process();
+        TextWriter _standardInput;
+        public TextWriter StandardInput
+        {
+            get => _standardInput;
+            set => _standardInput = value;
+        }
+        TextReader tr;
+        public void StartProcess()
+        {
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardInput = true;
+            processInfo.RedirectStandardOutput = true;
+          
+            p.StartInfo = processInfo;
+            p.Start();
+            StandardInput = p.StandardInput;
+            // tr = p.StandardOutput;
+            p.OutputDataReceived += P_OutputDataReceived;
+            p.ErrorDataReceived += P_ErrorDataReceived; 
+            p.
+            p.BeginOutputReadLine();
+            p.BeginErrorReadLine();
+            p.WaitForExit();
+        }
+
+        private  void P_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string error = e.Data;
+            Console.WriteLine(error);
+        }
+
+        private  void P_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string data = e.Data;
+            Console.WriteLine(data);
+            if(data.Trim().ToLower().Contains("Select your Environment".ToLower()))
+            {
+                string userinput = Console.ReadLine();
+                this.StandardInput.WriteLine(userinput);
+            }
+            if (data.Trim().ToLower().Contains("Liquibase Successfully Updated".ToLower()))
+            {
+                string userinput = Console.ReadKey().KeyChar.ToString();
+                this.StandardInput.WriteLine(userinput);
+            }
+
+            
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ExecuteLiquibase().StartProcess();
+        }
+
+    }
+}
